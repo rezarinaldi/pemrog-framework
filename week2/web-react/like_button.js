5acf75e2eda5208b546efd62e4510ab39831ba26
rezarinaldi <rez4rinaldi@gmail.com>
2021-02-22 09:55:05 +0700

    upload tugas week 2

---
+++ week2/web-react/like_button.js
@@ -0,0 +1,20 @@
+// ... the starter code you pasted ...
+const e = React.createElement;
+
+function LikeButton() {
+  //Display a "Like" <button>
+  return e(
+    "button",
+    {
+      onClick: () => alert("berhasil"),
+    },
+    "Like"
+  );
+}
+
+// const button = () => {
+//     return <button>Like</button>
+// }
+
+const domContainer = document.querySelector("#like_button_container");
+ReactDOM.render(e(LikeButton), domContainer);
