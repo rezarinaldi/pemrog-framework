ab3b017b9c8579e4cf083e2ecad8aff63088aadc
rezarinaldi <rez4rinaldi@gmail.com>
2021-04-14 02:39:41 +0700

    upload tugas week 10

---
+++ week10/react-firebase/src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCcQp4Cuk_4qe6v4MFf88_PEOvuBccvrMM",
+  authDomain: "pbf-modul10.firebaseapp.com",
+  projectId: "pbf-modul10",
+  storageBucket: "pbf-modul10.appspot.com",
+  messagingSenderId: "655614933412",
+  appId: "1:655614933412:web:e6c845fb23ca42cbb2a931",
+  measurementId: "G-8R360JK63R",
+};
---
+++ week10/react-firebase/src/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import "./style.css";
+import "tailwindcss/tailwind.css";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./Header";
+import routes from "./routes";
+import firebase from "firebase";
+import firebaseConfig from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+      Is logged in? {JSON.stringify(isLoggedIn)}
+      <div className="App">
+        <Router>
+          <Header />
+
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
